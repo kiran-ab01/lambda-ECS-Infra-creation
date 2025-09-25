d123ac1b6a0f7c708c70de9badbcfac0148a994c
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-09-25 18:16:18 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -20,25 +20,7 @@ data "aws_subnets" "default_vpc_subnets" {
 # ------------------------
 # Security Group (for HTTP)
 # ------------------------
-resource "aws_security_group" "ecs_sg" {
-  name        = "ecs-sg_new"
-  description = "Allow HTTP"
-  vpc_id      = data.aws_vpc.default.id
- 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+
  
 # ------------------------
 # ECS Cluster
