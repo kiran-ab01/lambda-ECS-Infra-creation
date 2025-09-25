ac1d7a7d5c941d8bf2956e3c51664bd3b7cd18ad
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-09-25 16:38:38 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -32,7 +32,5 @@ resource "aws_lambda_function" "hello" {
   role          = aws_iam_role.lambda_exec.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
- 
-  filename         = "lambda_function_payload.zip"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+
 }
